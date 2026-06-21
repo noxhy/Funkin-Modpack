2c82d0ce023f4ab592525ae99ad42276d756c0c5
noxhy <58011428+noxhy@users.noreply.github.com>
2026-06-21 04:04:49 -0400

    modpack

---
+++ FunkinCrew-FnF/init.gd
@@ -0,0 +1,29 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Preload.character_data["boyfriend"] = load("uid://c73l5wk1k58mp")
+	Preload.character_data["pico"] = load("uid://dxqhmrv2rrdkc")
+	
+	GameManager.current_character = "boyfriend"
+	GameManager.character = Preload.character_data.get(GameManager.current_character)
+	
+	SoundManager.music.stream = load("uid://du4qr2rxqjkw")
+	SoundManager.accept.stream = load("uid://guk2nlj2xwt7")
+	SoundManager.cancel.stream = load("uid://dyy2yik0yf60r")
+	SoundManager.scroll.stream = load("uid://svn606p6k0w0")
+	
+	var miss_sound: AudioStreamRandomizer = AudioStreamRandomizer.new()
+	miss_sound.streams_count = 3
+	miss_sound.add_stream(0, load("uid://bmggbtser1g3u"))
+	miss_sound.add_stream(1, load("uid://cfnrxgladhi2w"))
+	miss_sound.add_stream(2, load("uid://cqne25yeqb7op"))
+	
+	SoundManager.miss.stream = miss_sound
+	SoundManager.anti_spam.stream = miss_sound
+	SoundManager.anti_spam.volume_db = -8
+	
+	DisplayServer.set_icon(load("uid://b6tvkvdbdai2u").get_image())
+	
+	Constants.set_script(preload("res://FunkinCrew-FnF/constants.gd"))
+	Global.change_scene_to("uid://b1kmgjxpce1de", null, false)
